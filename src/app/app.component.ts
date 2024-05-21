e4c6ad6ca936fdcc23a2e36439a3c716413de661
XINZESONG <otisxzsong@gmail.com>
2024-05-21 17:29:47 +1000

    test basic css and ngstyle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular';
+
+  calculateStyles() {
+    return {'color': 'red',  'position':'absolute', 'right':'10%','top':'20%'}
+  }
 }
