d8da1804b7062b1a963f9fa54250515bc2c114c5
XINZESONG <otisxzsong@gmail.com>
2024-05-23 17:24:29 +1000

    create home page and user page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component} from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
-
+import { UserComponent } from './user/user.component';
+import { HomeComponent } from './home/home.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule,UserComponent, HomeComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular';
@@ -17,7 +18,14 @@ export class AppComponent {
   position_X = 100/this.width*100;
   position_Y = 35/this.height*100;
 
+  constructor(private router:Router){}
+
   calculateStyles() {
     return {'position':'absolute', 'right':this.position_X+'%','top':this.position_Y+'%'}
+  }
+
+  goToPage(pageName:string):void{
+    console.log(pageName)
+    this.router.navigate([`${pageName}`])
   }
 }
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { RouterOutlet, Router } from '@angular/router';
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+  constructor(private router:Router){}
+
+  goToPage(pageName:string):void{
+    console.log(pageName)
+    this.router.navigate([`${pageName}`])
+  }
+}
